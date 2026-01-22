540b0694c8ec742a21b7adb33fbcae57c45383ec
Ryoma Hara <toooooom720@gmail.com>
2026-01-22 23:54:40 +0900

    add: nvim-lspconfig plugin

--- common/nvim/init.lua
+++ common/nvim/init.lua
@@ -1,6 +1,12 @@
 vim.opt.number = true
 vim.opt.cursorline = true
 
+local vim = vim
+local Plug = vim.fn['plug#']
 
+vim.call('plug#begin')
 
+  Plug 'neovim/nvim-lspconfig'
+
+vim.call('plug#end')
 
