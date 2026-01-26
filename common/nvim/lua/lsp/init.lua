4ca8d0059aae61add32a6ce98a13ef8c8560006e
Ryoma Hara <toooooom720@gmail.com>
2026-01-26 21:57:42 +0900

    chore: Add keybindings

--- common/nvim/lua/lsp/init.lua
+++ common/nvim/lua/lsp/init.lua
@@ -5,4 +5,19 @@ local lsp_names = {
 
 vim.lsp.enable(lsp_names)
 
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(ev)
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts) -- 定義へジャンプ
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)  -- 宣言へジャンプ
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts) -- 実装を一覧表示
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts) -- 参照箇所を一覧表示
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts) -- 前の指摘へ
+    vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts) -- 次の指摘へ
+  end,
+})
+
 
